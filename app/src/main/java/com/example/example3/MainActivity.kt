4747cf68e2d8f3bfaa715e3c8be7cedb094e9b84
NellyBelikova <nelly.belikova@gmail.com>
2022-12-21 21:25:08 +0300

    rd /s /q ".git"

---
+++ app/src/main/java/com/example/example3/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.example3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val addButton = findViewById<Button>(R.id.button)
+        val linear = findViewById<LinearLayout>(R.id.linear)
+        addButton.setOnClickListener {
+            counter++
+            val marker = findViewById<EditText>(R.id.marker)
+            linear.addView(marker)
+            //if you want to identify the created editTexts, set a tag, like below
+            marker.tag = "Marker$counter"
+            marker.hint = "Marker$counter"
+        }
+
+
+    }
+
+}
+
